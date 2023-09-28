e48c5f0a9421171a5ca25a1857db70e375382ca3
Rafael Rossi <rossirafael@yahoo.com>
2023-09-28 14:39:25 +0200

    Game modes in GUI

--- app.py
+++ app.py
@@ -48,21 +48,120 @@ class Window(QtWidgets.QWidget):
 
         self.code = "cymb"
 
-        self.guesses = [list("oooo") for _ in range(12)]
+        self.guesses = [list("rrgg")]
+        self.guesses.extend([list("oooo") for _ in range(11)])
         self.responses = [list("oooo") for _ in range(12)]
         self.S = list(itertools.product(mm.PEGS, repeat=4))
         self.T = self.S[:]
 
     def play(self):
-        def validate(g):
+        if self.game_mode == "auto":
+            self.computer_guess()
+            resp = self.computer_response()
+            if self.check_won(resp):
+                return
+
+        elif self.game_mode == "breaker":
+            if not self.human_guess():
+                print("Invalid guess. Try again.")
+                return
+            resp = self.computer_response()
+            if self.check_won(resp):
+                return
+
+        elif self.game_mode == "keeper":
+            resp = self.human_response()
+            if not resp:
+                print("Invalid response. Try again.")
+                return
+            if self.check_won(resp):
+                return
+            self.active_row += 1
+            self.computer_guess()
+            self.update()
+            return
+
+        self.active_row += 1
+        self.update()
+
+    def check_won(self, resp):
+        blacks, whites = resp
+        if blacks == 4:
+            print(f"The code is {''.join(self.guesses[self.active_row])}/{self.code}.")
+            self.active_row = -1
+            return True
+        return False
+
+    def human_guess(self):
+        """Validate a user input guess"""
+        assert self.game_mode == "breaker"
+        g = "".join(self.guesses[self.active_row])
+        return len(g) == 4 and set(g).issubset(set(mm.PEGS))
+
+    def computer_guess(self):
+        assert self.game_mode in {"auto", "keeper"}
+
+        if self.active_row == 0:
+            return
+
+        last_guess = "".join(self.guesses[self.active_row - 1])
+        resp_str = self.responses[self.active_row - 1]
+        resp = resp_str.count("k"), resp_str.count("w")
+        mm.prune(self.S, self.T, last_guess, resp, verbose=False)
+        next_guess = self.guesses[self.active_row]
+        next_guess[:] = mm.get_next_guess(self.S, self.T, verbose=False)
+
+    def human_response(self):
+        """Validate a user input response"""
+        assert self.game_mode == "keeper"
+
+        resp_str = self.responses[self.active_row]
+        resp = resp_str.count("k"), resp_str.count("w")
+        blacks, whites = resp
+        if (
+            0 <= blacks <= 4
+            and 0 <= whites <= 4
+            and whites + blacks <= 4
+            and not (blacks == 3 and whites == 1)
+        ):
+            return blacks, whites
+        return False
+
+    def computer_response(self):
+        assert self.game_mode in {"auto", "breaker"}
+
+        guess = "".join(self.guesses[self.active_row])
+        resp = mm.get_response(guess, self.code)
+        blacks, whites = resp
+        self.responses[self.active_row] = (
+            "k" * blacks + "w" * whites + "o" * (4 - blacks - whites)
+        )
+        return blacks, whites
+
+    def old_play_old(self):
+        def validate_guess(g):
             """Validate a user input guess"""
             return len(g) == 4 and set(g).issubset(set(mm.PEGS))
 
+        def validate_response(r):
+            """Validate a user input response"""
+            blacks, whites = r
+            return (
+                0 <= blacks <= 4
+                and 0 <= whites <= 4
+                and whites + blacks <= 4
+                and not (blacks == 3 and whites == 1)
+            )
+
         if self.game_mode in {"auto", "keeper"} and self.active_row == 0:
             self.guesses[self.active_row] = list("rrgg")
+            if self.game_mode == "keeper":
+                self.active_row += 1
+                self.update()
+                return
 
         guess = "".join(self.guesses[self.active_row])
-        if not validate(guess):
+        if not validate_guess(guess):
             print("Invalid guess. Try again.")
             return
 
@@ -78,6 +177,9 @@ class Window(QtWidgets.QWidget):
         else:  # self.game_mode == 'keeper':
             resp_str = self.responses[self.active_row]
             resp = resp_str.count("k"), resp_str.count("w")
+            if not validate_response(resp):
+                print("Invalid response. Try again.")
+                return
             blacks, whites = resp
 
         print(f"-> {blacks=}, {whites=}")
@@ -129,17 +231,33 @@ class Window(QtWidgets.QWidget):
 
     def mouse_clicked(self, event):
         print("mouse_clicked() called")
+
+        if self.game_mode == "auto":
+            return
+
         x = event.pos().x()
         y = event.pos().y()
         row = y // self.peg_box_size
         col = x // self.peg_box_size
-        print(f"clicked round {row+1}, peg {col+1}")
 
-        if not row == self.active_row or self.active_row == -1 or col > 3:
+        if not row == self.active_row or self.active_row == -1:
             return
 
-        self.guesses[row][col] = PEGS[PEGS.find(self.guesses[row][col]) + 1]
-        print(f"{''.join(self.guesses[row])}")
+        if self.game_mode == "breaker" and col < 4:
+            # switch between the colors
+            print(f"clicked round {row+1}, peg {col+1}")
+            self.guesses[row][col] = PEGS[PEGS.find(self.guesses[row][col]) + 1]
+            print(f"{''.join(self.guesses[row])}")
+
+        elif self.game_mode == "keeper" and col == 4:
+            xi = x - 4 * self.peg_box_size
+            yi = y - self.active_row * self.peg_box_size
+            coli = xi // (self.response_box_size / 2)
+            rowi = yi // (self.response_box_size / 2)
+            idx = int(coli + 2 * rowi)
+            print(f"clicked response {row+1}, peg {idx+1}")
+            self.responses[row][idx] = RESP[RESP.find(self.responses[row][idx]) + 1]
+
         self.update()
 
     def keyPressEvent(self, e):
@@ -200,11 +318,13 @@ class Window(QtWidgets.QWidget):
         # draw the board borders
         pen.setWidth(5)
 
-        # draw the pegs
+        # draw the pegs and responsed
         for row in range(12):
             guess = self.guesses[row]
             response = self.responses[row]
             y = row * self.peg_box_size + self.pegs_pad
+
+            # draw the guesses pegs
             for col in range(4):
                 x = col * self.peg_box_size + self.pegs_pad
                 s = self.peg_size
@@ -213,6 +333,7 @@ class Window(QtWidgets.QWidget):
 
             x = 4 * self.peg_box_size + self.pegs_pad
             i = 0
+            # draw the response pegs
             for r in range(2):
                 y1 = y + r * self.response_size + r * self.resp_pad
                 for c in range(2):
